3215eb7e2c215b2abaef0c0f9f92ad6bd6192b41
maroms <maromshpak094@gmail.com>
2024-09-13 14:41:11 +0300

    feat:implementaions(need to be fixed)

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,10 +1,24 @@
 #include "io.h"
 #include "shell.h"
+#include "memory.h"
 
 #define INPUT_BUFFER_SIZE 256 
 
 void __attribute__((section(".entry"))) kernel_main(uint16_t drive_id)
 {
+
+    init_memory();
+
+    void *block1 = kernel_malloc(10);  // allocate 8 kb
+    if (block1) {
+        puts("Allocated block1 at");
+    } else {
+        puts("Failed to allocate block1");
+    }
+
+    kfree(block1, 8192);  
+    puts("Freed block1");
+    
     while(true)
     {
       char input_buffer[INPUT_BUFFER_SIZE];
--- kernel/src/memory.h
+++ kernel/src/memory.h
@@ -1,6 +1,6 @@
 #pragma once
 
-void *memmove(void *dest, const void *src, int len);
+void *memmove(void *dest, const void *src, int len);    
 
 void *memset(void *dest, int val, int len);
 
@@ -8,8 +8,14 @@ void init_memory();
 
 void *kernel_malloc(int size);
 
-void kfree(void *pointer);
+void kfree(void *pointer , int size);
 
 void init_paging();
 
 void handle_page_fault();
+
+//malloc functions
+void set_bit(int bit);
+void clear_bit(int bit); 
+int test_bit(int bit);
+
