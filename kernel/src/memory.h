6b15f13401a617582c7752742424f3b95baf04e2
maroms <maromshpak094@gmail.com>
2024-09-13 10:09:55 +0300

    feat: memory.h functions to implement

--- kernel/src/memory.h
+++ kernel/src/memory.h
@@ -3,3 +3,13 @@
 void *memmove(void *dest, const void *src, int len);
 
 void *memset(void *dest, int val, int len);
+
+void init_memory();
+
+void *kernel_malloc(int size);
+
+void kfree(void *pointer);
+
+void init_paging();
+
+void handle_page_fault();
