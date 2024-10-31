7394fe32c6c7695e1c36f16631a9b848eb899e81
Denloob <den@denloob.uk>
2024-10-31 09:26:08 +0200

    feat: add macros to store and restore scratch registers

--- kernel/src/isr.h
+++ kernel/src/isr.h
@@ -9,3 +9,32 @@ typedef struct
     uint64_t rsp;
     uint64_t ss;
 } isr_InterruptFrame;
+
+// TODO: store xmm registers
+#define PUSH_CALLER_STORED()                                                   \
+        asm volatile("                                                         \
+            push rax;                                                          \
+            push rcx;                                                          \
+            push rdx;                                                          \
+            push rdi;                                                          \
+            push rsi;                                                          \
+            push r8;                                                           \
+            push r9;                                                           \
+            push r10;                                                          \
+            push r11;                                                          \
+        " : : : "memory")
+
+// TODO: restore xmm registers
+#define POP_CALLER_STORED()                                                    \
+        asm volatile("                                                         \
+            pop r11;                                                           \
+            pop r10;                                                           \
+            pop r9;                                                            \
+            pop r8;                                                            \
+            pop rsi;                                                           \
+            pop rdi;                                                           \
+            pop rdx;                                                           \
+            pop rcx;                                                           \
+            pop rax;                                                           \
+        " : : : "memory")
+
