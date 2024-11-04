90fd950b7f6548385c08d6f79118631a3ec4c202
Denloob <den@denloob.uk>
2024-11-05 00:50:36 +0200

    feat: add color palette command

--- kernel/src/shell.c
+++ kernel/src/shell.c
@@ -1,5 +1,7 @@
+#include "memory.h"
 #include "string.h"
 #include "shell.h"
+#include "vga.h"
 
 #define ACPI_SHUTDOWN 0x2000
 
@@ -50,6 +52,48 @@ void clear_command()
     io_clear_vga();
 }
 
+static void draw_square(int x, int y, int size, int color)
+{
+    for (int i = 0; i < size; i++)
+    {
+        memset((uint8_t *)VGA_GRAPHICS_ADDRESS + (y + i)* VGA_GRAPHICS_WIDTH + x, color, size);
+    }
+}
+
+void color_command()
+{
+    io_clear_vga();
+    const char target_key = 'q';
+    const char target_key_upper = target_key | ('a' - 'A');
+    printf("To exit the color palette, press '%c'.\n", target_key_upper);
+    printf("Press '%c' to enter the palette...", target_key_upper);
+
+    int key;
+    while (key_to_char(io_input_keyboard_key()) != target_key);
+    vga_mode_graphics();
+    memset((uint8_t *)VGA_GRAPHICS_ADDRESS, 0xf, VGA_GRAPHICS_WIDTH*VGA_GRAPHICS_HEIGHT);
+
+    const int square_size = 10;
+    const int space = 2;
+
+    for (int x_idx = 0; x_idx < 0x10; x_idx++)
+    {
+        for (int y_idx = 0; y_idx < 0x10; y_idx++)
+        {
+            const int x = space + x_idx * (square_size + space);
+            const int y = space + y_idx * (square_size + space);
+            const uint8_t color = y_idx << 4 | x_idx;
+
+            draw_square(x, y, square_size, color);
+        }
+    }
+
+    while (key_to_char(io_input_keyboard_key()) != target_key);
+    // TODO: returning back doesn't work currently, I'll fix later
+    vga_mode_text();
+    io_clear_vga();
+}
+
 void parse_command(char* command , int max_size)
 {
   if(compare_strings("shutdown" , command))
@@ -67,6 +111,10 @@ void parse_command(char* command , int max_size)
   else if(compare_strings("clear" , command))
   {
     clear_command();
+  }
+  else if(compare_strings("color" , command))
+  {
+    color_command();
   }
   else
   {
