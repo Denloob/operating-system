36650e6be2abd40a76f082eb946f3013331f000a
Denloob <den@denloob.uk>
2024-11-08 13:37:51 +0200

    feat: add min/max macros

--- lib/math.h
+++ lib/math.h
@@ -2,3 +2,6 @@
 
 #define math_ALIGN_UP(value, boundary)   (((value) + boundary - 1) & ~(boundary - 1))
 #define math_ALIGN_DOWN(value, boundary) ((value) & ~(boundary - 1))
+
+#define MIN(x, y) ((x) > (y) ? (y) : (x))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
