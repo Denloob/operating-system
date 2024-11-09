83b34a6a20840c02e8a5a7fb97610a4d217482fb
Denloob <den@denloob.uk>
2024-11-09 14:30:09 +0200

    feat: add strcasecmp and strncasecmp

--- lib/string.c
+++ lib/string.c
@@ -1,4 +1,5 @@
 #include "string.h"
+#include "types.h"
 
 int strlen(const char *s)
 {
@@ -9,4 +10,38 @@ int strlen(const char *s)
     }
 
     return len;
+}
+
+#define TOLOWER(c) (c | ('a' - 'A'))
+
+int strcasecmp(const char *s1, const char *s2)
+{
+    int result = 0;
+
+    if (s1 == s2)
+        return 0;
+
+    while (( result = TOLOWER(*s1) - TOLOWER(*s2++)) == 0)
+    {
+        if (*s1++ == '\0')
+            break;
+    }
+
+    return result;
+}
+
+int strncasecmp(const char *s1, const char *s2, size_t n)
+{
+    int result = 0;
+
+    if (s1 == s2 || n == 0)
+        return 0;
+
+    while (( result = TOLOWER(*s1) - TOLOWER(*s2++)) == 0)
+    {
+        if (*s1++ == '\0' || --n == 0)
+            break;
+    }
+
+    return result;
 }
--- lib/string.h
+++ lib/string.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#include "types.h"
+
 int strlen(const char *s);
+int strcasecmp(const char *s1, const char *s2);
+int strncasecmp(const char *s1, const char *s2, size_t n);
