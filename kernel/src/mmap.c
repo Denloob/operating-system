d6fbccda99f66398d3263a103baeb2800b3971af
Denloob <den@denloob.uk>
2024-11-10 19:06:49 +0200

    feat: add mmap

---
+++ kernel/src/mmap.c
@@ -0,0 +1,84 @@
+#include "mmap.h"
+#include "range.h"
+#include "assert.h"
+#include "mmu.h"
+#include "math.h"
+#include "res.h"
+
+#define PAGE_SIZE 0x1000
+#define PAGE_ALIGN_UP(address)   math_ALIGN_UP(address, PAGE_SIZE)
+#define PAGE_ALIGN_DOWN(address) math_ALIGN_DOWN(address, PAGE_SIZE)
+
+struct {
+    range_Range *base;
+    uint64_t length;
+} g_phys_memory_map = {0}; // Init with 0 so it's placed in .data and not in .bss
+
+void mmap_init(range_Range *mmap_base, uint64_t length)
+{
+    g_phys_memory_map.base = mmap_base;
+    g_phys_memory_map.length = length;
+}
+
+/**
+ * @brief - Allocate physical memory of size `0 < result_size < want_size`, page
+ *              aligned.
+ *
+ * @param want_size            - The size you want to allocate. The allocated
+ *                                  memory will never be larger than this.
+ *                                  Must be page aligned
+ * @param out_result[out]      - The start address of the allocated physical memory.
+ * @param out_result_size[out] - The size of the allocated physical memory.
+ *
+ * @return - true on success, false otherwise. If the function didn't succeed,
+ *              this means that there's no more free physical RAM available of
+ *              any size.
+ */
+bool allocate_physical_memory(uint64_t want_size, uint64_t *out_result, uint64_t *out_result_size)
+{
+    assert(g_phys_memory_map.base);
+
+    return range_pop_of_size_or_less(g_phys_memory_map.base, g_phys_memory_map.length,
+                                     want_size, out_result, out_result_size);
+}
+
+int prot_to_mmu_flags(mmap_Protection prot)
+{
+    int flags = 0;
+
+    if ((prot & MMAP_PROT_EXEC) == 0)  flags |= MMU_EXECUTE_DISABLE;
+    if ((prot & MMAP_PROT_WRITE) != 0) flags |= MMU_READ_WRITE;
+
+    return flags;
+}
+
+res mmap(void *addr, size_t size, mmap_Protection prot)
+{
+    assert(g_phys_memory_map.base);
+
+    if ((prot & MMAP_PROT_READ) == 0) return res_mmap_MUST_BE_READABLE;
+    const int mmu_flags = prot_to_mmu_flags(prot);
+
+    if (size == 0) return res_mmap_INVALID_SIZE;
+
+    uint64_t size_aligned = PAGE_ALIGN_UP(size);
+    uint64_t addr_it = (uint64_t)addr;
+
+    while (size_aligned)
+    {
+        uint64_t allocated_phys_addr;
+        uint64_t allocated_size;
+        bool success = allocate_physical_memory(size_aligned,
+                                                &allocated_phys_addr, &allocated_size);
+        if (!success) return res_mmap_OUT_OF_MEMORY;
+
+        uint64_t begin = allocated_phys_addr;
+        uint64_t end   = begin + allocated_size;
+        mmu_map_range(begin, end, addr_it, mmu_flags);
+
+        size_aligned -= allocated_size;
+        addr_it += allocated_size;
+    }
+
+    return res_OK;
+}
---
+++ kernel/src/mmap.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+#include "range.h"
+#include "res.h"
+
+typedef enum {
+    MMAP_PROT_NONE  = 0x0,
+    MMAP_PROT_READ  = 0x1,
+    MMAP_PROT_WRITE = 0x2,
+    MMAP_PROT_EXEC  = 0x4,
+} mmap_Protection;
+
+#define res_mmap_OUT_OF_MEMORY    "cannot allocate memory"
+#define res_mmap_INVALID_SIZE     "size cannot be 0"
+#define res_mmap_MUST_BE_READABLE "non readable prot is not supported"
+
+/**
+ * @brief Initialize the mmap memory map
+ *
+ * @param mmap_base The memory map base
+ * @param length The length of the memory map
+ * @note  The memory map was most likely passed to the kernel by the bootloader.
+ */
+void mmap_init(range_Range *mmap_base, uint64_t length);
+
+/**
+ * @brief Map a memory region starting from address of size `size` with `prot`
+ *          protections.
+ *
+ * @param addr The beginning of the (virtual) memory region.
+ * @param size The size of the region.
+ * @param prot The protections for the region.
+ * @return res_OK or one of the errors defined above.
+ */
+res mmap(void *addr, size_t size, mmap_Protection prot);
