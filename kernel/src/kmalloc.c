c6e115eb73052951171c225122f8443454a7a6b6
Denloob <den@denloob.uk>
2024-11-14 23:28:00 +0200

    feat: implement allocating freed chunks

--- kernel/src/kmalloc.c
+++ kernel/src/kmalloc.c
@@ -18,8 +18,8 @@ struct malloc_chunk
     size_t prev_chunk_size;        // Size of previous chunk (accessible only if prev's free).
     size_t chunk_size;             // Size of this chunk. 8 byte aligned. Lower 3 bytes used as prev chunk status
 
-    struct malloc_chunk *bk;       // When current chunk is free, these two act as a linked list of free chunks.
-    struct malloc_chunk *fd;
+    struct malloc_chunk *fd;       // When current chunk is free, these two act as a linked list of free chunks.
+    struct malloc_chunk *bk;
 };
 
 // Offset from malloc_chunk to the memory region accessible by user when chunk is
@@ -33,7 +33,7 @@ enum {
 };
 
 #define addr2chunk(chunk) (malloc_chunk *)((void *)(chunk) - CHUNK_HEADER_SIZE)
-#define chunk2addr(chunk) (void *)(&((chunk)->bk))
+#define chunk2addr(chunk) (void *)(&((chunk)->fd))
 
 #define MIN_CHUNK_SIZE          (sizeof(malloc_chunk))
 #define CHUNK_SIZE_ALIGN        (CHUNK_HEADER_SIZE)
@@ -47,14 +47,65 @@ malloc_chunk *next_chunk(malloc_chunk *chunk)
      return ((void *)(chunk) + chunk_size(chunk));
 }
 
+// Remove `chunk` from a linked list via its `fd` and `bk`.
+void unlink_chunk(malloc_chunk *chunk)
+{
+    assert(chunk->fd->bk == chunk && chunk->bk->fd == chunk && "chunk not in a valid linked list");
+    malloc_chunk *fd = chunk->fd;
+    malloc_chunk *bk = chunk->bk;
+
+    chunk->bk->fd = fd;
+    chunk->fd->bk = bk;
+}
+
 /*
                    Internal State Managing
 */
 
+
+/* Bins
+ * ----
+ * Each bin in an arena represents a doubly linked list of free chunks of a
+ * particular size range. That is, if a chunk of a specific size is needed,
+ * a corresponding bin can be used to quickly get an address that is currently
+ * not in use.
+ *
+ * The first bin is an "unsorted bin", i.e. it contains chunks of all sizes.
+ * these chunks are placed there by `free` function, and later malloc will pop
+ * them, and either use them if they match the request, or sort them into the
+ * correct bin.
+ *
+ * To save some space and logic, the bins are stored as 2 pointers in the arena,
+ * but accessed as if they represented an actual chunk (using some pointer
+ * repositioning tricks).
+ */
+
+#define BIN_COUNT 1
+typedef malloc_chunk malloc_bin;
+
+// Get the bin at the index `idx` in an arena.
+#define bin_at(arena, idx)                                                     \
+    (malloc_bin *)(((char *)&((arena)->bins[(idx)]))                           \
+                    - offsetof(malloc_chunk, fd))
+// TODO: support more bins than just unsorted bin
+
 struct malloc_state
 {
     malloc_chunk *top; // The top free chunk
+    size_t _pad; // Padding so bins don't index out of the arena bounds
+    struct {
+        malloc_chunk *fd;
+        malloc_chunk *bk;
+    } bins[BIN_COUNT];
 };
+
+void bin_insert(malloc_bin *bin, malloc_chunk *chunk)
+{
+    chunk->fd = bin->fd;
+    chunk->bk = bin;
+    bin->fd->bk = chunk; // On first insert, when bin->fd == bin, will set bin->bk to be `chunk`
+    bin->fd = chunk;
+}
 
 static malloc_state main_arena;
 static bool is_malloc_initialized = false;
@@ -69,6 +120,13 @@ static void malloc_initialize()
 
     main_arena.top = allocated_addr;
     main_arena.top->chunk_size = PAGE_SIZE | MALLOC_CHUNK_PREV_IN_USE;
+
+    // Initialize circular links for the bins
+    for (int i = 0; i < BIN_COUNT; i++)
+    {
+        malloc_bin *bin = bin_at(&main_arena, i);
+        bin->fd = bin->bk = bin;
+    }
 }
 
 /**
@@ -96,6 +154,46 @@ WUR static res malloc_grow_heap(size_t wanted_size)
     return rs;
 }
 
+void *malloc_from_top(size_t victim_size)
+{
+    size_t top_size = main_arena.top->chunk_size;
+    if (top_size < victim_size)
+    {
+        res rs = malloc_grow_heap(victim_size);
+        if (IS_ERR(rs))
+            return NULL;
+    }
+
+    malloc_chunk *victim = main_arena.top;
+
+    victim->chunk_size = victim_size | (top_size & MALLOC_CHUNK_PREV_IN_USE);
+    main_arena.top = next_chunk(victim);
+    main_arena.top->chunk_size = (top_size - victim_size) | MALLOC_CHUNK_PREV_IN_USE;
+
+    return chunk2addr(victim);
+}
+
+void *malloc_from_bin(size_t victim_size)
+{
+    malloc_bin *bin = bin_at(&main_arena, 0);
+    malloc_chunk *cur = bin->fd;
+
+    while (cur != bin)
+    {
+        if (chunk_size(cur) <= victim_size)
+        {
+            unlink_chunk(cur);
+
+            malloc_chunk *next = next_chunk(cur);
+            next->chunk_size |= MALLOC_CHUNK_PREV_IN_USE;
+
+            return cur;
+        }
+    }
+
+    return NULL;
+}
+
 /*
                    kmalloc / kfree
 */
@@ -110,22 +208,13 @@ void *kmalloc(size_t size)
     size_t victim_size = math_ALIGN_UP(size + CHUNK_PARTIAL_HEADER_SIZE, CHUNK_SIZE_ALIGN);
     if (victim_size < MIN_CHUNK_SIZE) victim_size = MIN_CHUNK_SIZE;
 
-    size_t top_size = main_arena.top->chunk_size;
-    if (top_size < victim_size)
+    void *victim = malloc_from_bin(victim_size);
+    if (victim != NULL)
     {
-        res rs = malloc_grow_heap(victim_size);
-        if (IS_ERR(rs))
-            return NULL;
+        return victim;
     }
 
-    // Allocate a chunk from the top of the heap
-    malloc_chunk *victim = main_arena.top;
-
-    victim->chunk_size = victim_size | (top_size & MALLOC_CHUNK_PREV_IN_USE);
-    main_arena.top = next_chunk(victim);
-    main_arena.top->chunk_size = (top_size - victim_size) | MALLOC_CHUNK_PREV_IN_USE;
-
-    return chunk2addr(victim);
+    return malloc_from_top(victim_size);
 }
 
 void kfree(void *addr)
@@ -136,8 +225,12 @@ void kfree(void *addr)
     malloc_chunk *next = next_chunk(chunk);
     assert((next->chunk_size & MALLOC_CHUNK_PREV_IN_USE) == 1 && "next chunk doesn't think this one exists");
 
-    // TODO: set fd and bk
-
     next->prev_chunk_size = chunk_size(chunk);
     next->chunk_size &= (~MALLOC_CHUNK_PREV_IN_USE);
+
+    // TODO: consolidate if possible
+
+    malloc_bin *unsorted_bin = bin_at(&main_arena, 0);
+
+    bin_insert(unsorted_bin, chunk);
 }
