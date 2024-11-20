637c7fb11213e9f549d00ccaf54d802fae23a3b0
maroms <maromshpak094@gmail.com>
2024-11-20 22:03:57 +0200

    feat: drive_write place holder

--- bootloader/stage2/src/drive.h
+++ bootloader/stage2/src/drive.h
@@ -2,7 +2,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-
+#include "assert.h"
 typedef struct Drive {
     uint8_t id;
     uint8_t type;
@@ -26,3 +26,8 @@ void drive_lba_to_chs(Drive *drive, uint32_t lba, drive_CHS *ret);
 bool drive_init(Drive *drive, uint16_t drive_id);
 
 bool drive_read(Drive *drive, uint64_t address, uint8_t *buffer, uint32_t size);
+
+static inline bool drive_write(Drive *drive, uint64_t address, uint8_t *buffer, uint32_t size)
+{
+    assert(false && "not supported");
+};
