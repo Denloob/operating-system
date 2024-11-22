5c0dece12b5fc3c6927dbb4cd77576d492bfb63c
Denloob <den@denloob.uk>
2024-11-22 17:58:24 +0200

    feat: zero out kernel registers before entering ring 3

--- kernel/src/usermode.c
+++ kernel/src/usermode.c
@@ -5,8 +5,25 @@ void usermode_jump_to(void *address, void *stack)
 {
     asm volatile (
         "mov r11, %[rflags]\n"
-        "mov rsp, %[stack]\n"
+        "mov rsp, %[stack]\n" // This must be before zeroing, so we don't zero the register with the value
         "mov rbp, rsp\n"
+
+        // Zero out all kernel registers (except the ones set later)
+        "xor rax, rax\n"
+        "xor rbx, rbx\n"
+        // skip rcx (which is the `address`)
+        "xor rdx, rdx\n"
+        "xor rsi, rsi\n"
+        "xor rdi, rdi\n"
+        // skip rbp and rsp
+        "xor r8, r8\n"
+        "xor r9, r9\n"
+        "xor r10, r10\n"
+        // skip r11
+        "xor r12, r12\n"
+        "xor r13, r13\n"
+        "xor r14, r14\n"
+        "xor r15, r15\n"
 
         "sysretq\n"
         :
