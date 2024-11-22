abab1887a4b3ba49b696a27ee79ff69000e2158c
Denloob <den@denloob.uk>
2024-11-22 17:00:31 +0200

    feat: add isr_CallerRegsFrame

--- kernel/src/isr.h
+++ kernel/src/isr.h
@@ -10,6 +10,20 @@ typedef struct
     uint64_t ss;
 } isr_InterruptFrame;
 
+
+// Frame for the registers pushed/poped by PUSH_CALLER_STORED/POP_CALLER_STORED
+typedef struct {
+    uint64_t r11;
+    uint64_t r10;
+    uint64_t r9;
+    uint64_t r8;
+    uint64_t rsi;
+    uint64_t rdi;
+    uint64_t rdx;
+    uint64_t rcx;
+    uint64_t rax;
+} isr_CallerRegsFrame;
+
 // TODO: store xmm registers
 #define PUSH_CALLER_STORED()                                                   \
         asm volatile("                                                         \
