a1c430efb0457c37780a8e9701db5a51a19fc89b
Denloob <den@denloob.uk>
2024-11-22 17:57:55 +0200

    feat: use sysret instead of iretq

--- kernel/src/usermode.c
+++ kernel/src/usermode.c
@@ -1,25 +1,17 @@
 #include "usermode.h"
+#include <stdint.h>
 
 void usermode_jump_to(void *address, void *stack)
 {
-    asm(
-        // Set the segment registers to segment 0x20 as usermode (data usermode segment)
-        "mov rax, 0x20 | 3 \n"
-        "mov ds, ax\n"
-        "mov es, ax\n"
-        "mov fs, ax\n"
-        "mov gs, ax\n"
+    asm volatile (
+        "mov r11, %[rflags]\n"
+        "mov rsp, %[stack]\n"
+        "mov rbp, rsp\n"
 
-        // Build in iretq frame
-        "push rax\n"        // SS
-        "push %[stack]\n"   // rsp
-        "pushf\n"           // FLAGS
-        "push 0x18 | 3\n"   // CS
-        "push %[address]\n" // rip
-        "iretq\n"
+        "sysretq\n"
         :
-        : [address] "D"(address), [stack] "S"(stack) // Put them in rdi and rsi
-        : "memory", "rax");
+        : "c"(address), [rflags] "i"(0x202), [stack] "r"(stack)
+        : "memory"); // We don't actually need to specify clobbers because it doesn't return. "memory" to force execution order.
 
     __builtin_unreachable();
 }
