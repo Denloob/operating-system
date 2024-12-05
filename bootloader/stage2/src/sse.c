e7f83de8a303c78ecbd88702d0ba0ffb71969407
Denloob <den@denloob.uk>
2024-12-05 16:39:55 +0200

    feat: enable SSE XSAVE

--- bootloader/stage2/src/sse.c
+++ bootloader/stage2/src/sse.c
@@ -1,4 +1,7 @@
 #include "sse.h"
+#include "assert.h"
+#include <stdbool.h>
+#include <stdint.h>
 
 #define CR0_MP         (1 << 1)
 #define CR0_EM         (1 << 2)
@@ -7,10 +10,23 @@
 #define CR0_NE         (1 << 5)
 #define CR4_OSFXSR     (1 << 9)  // 128bit SSE support enabled
 #define CR4_OSXMMEXCPT (1 << 10) // XF SSE exception enabled
-#define CR4_OSXSAVE    (1 << 18) // SSE state saving (we won't support it)
+#define CR4_OSXSAVE    (1 << 18) // SSE state saving
+
+#define CPUID_GETFEATURES 1
+#define CPUID_FEAT_ECX_XSAVE (1 << 26)
+
+static bool cpuid_check_xsave()
+{
+    uint32_t ecx;
+    asm("cpuid" : "=c"(ecx) : "a"(CPUID_GETFEATURES));
+
+    return ecx & CPUID_FEAT_ECX_XSAVE;
+}
 
 void sse_init()
 {
+    assert(cpuid_check_xsave() && "XSAVE must be supported by the CPU.");
+
     asm("mov eax, cr0\n"
         "and eax, %0\n"
         "or eax, %1\n"
@@ -23,7 +39,7 @@ void sse_init()
         "or eax, %0\n"
         "mov cr4, eax"
         :
-        : "i"(CR4_OSFXSR | CR4_OSXMMEXCPT)
+        : "i"(CR4_OSFXSR | CR4_OSXMMEXCPT | CR4_OSXSAVE)
         : "eax", "cc");
 
     asm("fninit\n");
