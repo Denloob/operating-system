15bf66a6b198974d03e643ad79d4dea1406bd9e4
Denloob <den@denloob.uk>
2024-12-09 12:33:41 +0200

    feat: implement context switching from a process to the kernel

--- kernel/src/pit.c
+++ kernel/src/pit.c
@@ -1,5 +1,8 @@
 #include "pit.h"
 #include "io.h"
+#include "isr.h"
+#include "schedular.h"
+#include "smartptr.h"
 #include <stdint.h>
 
 #define PIT_CHANNEL_0 0x40
@@ -14,11 +17,66 @@
 
 static volatile uint64_t g_pit_ms_counter;
 
+static void __attribute__((used, sysv_abi)) handle_special_time_event_impl()
+{
+}
+
+static volatile isr_InterruptFrame *g_interrupt_frame_ptr_tmp; // Temp place to store the pointer to isr_InterruptFrame.
+
+static void __attribute__((naked, used)) handle_special_time_event()
+{
+    // NOTE: we will not return to pit_isr_clock. We are now on our own. Although the timer was already
+    // incremented for us.
+    // Our special "time event" is a round robin interrupt, so we won't actually iretq back to the same process.
+    // Instead we will store all the current process data, and iretq to another process. So the schedular
+    // will be responsible for sending IRQ EOI.
+
+    asm volatile(
+                "mov %[g_interrupt_frame_ptr_tmp], rsp\n" // We haven't pushed anything yet, so rsp is actually the interrupt frame.
+                // 16 byte align the stack
+                "sub rsp, 8\n"
+                "and rsp, ~0xf\n"
+
+                // Put the `Regs` struct on to the stack. We will pass a pointer to it.
+
+                "push 0\n" // Skip RFALGS, will set them via the isr_InterruptFrame.
+                "push r15\n"
+                "push r14\n"
+                "push r13\n"
+                "push r12\n"
+                "push r11\n"
+                "push r10\n"
+                "push r9\n"
+                "push r8\n"
+                "push rdi\n"
+                "push rsi\n"
+                "push rbp\n"
+                "push 0\n" // Skip rsp, will set them via the isr_InterruptFrame.
+                "push rbx\n"
+                "push rdx\n"
+                "push rcx\n"
+                "push rax\n"
+                "push 0\n" // Padding before the SSE (stack aligned)
+
+                "sub rsp, 512\n"
+                "fxsave [rsp]\n"
+
+                "mov rdi, rsp\n"
+                "mov rsi, %[g_interrupt_frame_ptr_tmp]\n"
+
+                "call schedular_context_switch_from" // Does not return. We use call and not jmp to keep the stack 16 byte aligned.
+        : [g_interrupt_frame_ptr_tmp] "+m"(g_interrupt_frame_ptr_tmp) : : "memory");
+}
+
 void __attribute__((naked)) pit_isr_clock()
 {
-    asm volatile("push rax\n"
+    asm volatile(
+                "add %[ms_counter], 4\n"
 
-                 "add %[ms_counter], 4\n"
+                "test %[ms_counter], (2 << 2) - 1\n" // if (ms_counter % 8 == 0)
+                "jz handle_special_time_event" // Does not return
+
+                 "push rax\n"
 
                  "mov al, 0x20\n" // Send EOI to PIC
                  "out 0x20, al\n"
--- kernel/src/schedular.c
+++ kernel/src/schedular.c
@@ -1,10 +1,31 @@
+#include "isr.h"
 #include "pcb.h"
-#include "usermode.h"
+#include "regs.h"
 #include "schedular.h"
+#include "usermode.h"
+
+static uint64_t current_process_id;
 
 void schedular_context_switch_to(PCB *pcb)
 {
+    current_process_id = pcb->id;
     pcb->state = PCB_STATE_RUNNING;
     asm volatile("mov cr3, %0" : : "a"(pcb->paging) : "memory");
     usermode_jump_to((void *)pcb->rip, &pcb->regs);
+}
+
+void __attribute__((used, sysv_abi)) schedular_context_switch_from(Regs *regs,
+                                                         isr_InterruptFrame *frame)
+{
+    regs->rflags = frame->flags;
+    regs->rsp = frame->rsp;
+
+    // TODO: Get the pcb for current_process_id
+    PCB *pcb = NULL;
+
+    pcb->rip = frame->rip;
+    pcb->regs = *regs;
+    pcb->state = PCB_STATE_READY;
+
+    // TODO: choose next pcb and call schedular_context_switch_to with it
 }
