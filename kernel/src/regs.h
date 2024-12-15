87b2574f29c09525dc7b2997edb70f9323cc9dcf
Denloob <den@denloob.uk>
2024-12-15 21:26:27 +0200

    feat: regs PUSH and POP macros

--- kernel/src/regs.h
+++ kernel/src/regs.h
@@ -22,3 +22,56 @@ typedef struct {
     uint64_t rflags;
     __attribute__((aligned(16))) uint8_t sse[512];
 } Regs; // NOTE: change this struct very carefully, as its offsets are hardcoded in inline assembly for usermode jumps and possibly more.
+
+// Push current cpu registers onto the stack to get the full `Regs`. Stack must
+// be aligned before executing.
+// In the end, `$rsp` will contain the pointer `Regs`.
+#define regs_PUSH() asm volatile(                                              \
+                "sub rsp, 512\n"                                               \
+                "fxsave [rsp]\n"                                               \
+                "push 0\n" /* Padding before the SSE (stack aligned) */        \
+                                                                               \
+                "pushf\n"                                                      \
+                "push r15\n"                                                   \
+                "push r14\n"                                                   \
+                "push r13\n"                                                   \
+                "push r12\n"                                                   \
+                "push r11\n"                                                   \
+                "push r10\n"                                                   \
+                "push r9\n"                                                    \
+                "push r8\n"                                                    \
+                "push rdi\n"                                                   \
+                "push rsi\n"                                                   \
+                "push rbp\n"                                                   \
+                "push 0\n" /* Skip rsp, meaningless*/                          \
+                "push rbx\n"                                                   \
+                "push rdx\n"                                                   \
+                "push rcx\n"                                                   \
+                "push rax\n"                                                   \
+        ::: "memory")
+
+// pop the `Regs` struct at `$rsp` into the CPU registers.
+#define regs_POP() asm volatile(                                               \
+                "pop rax\n"                                                    \
+                "pop rcx\n"                                                    \
+                "pop rdx\n"                                                    \
+                "pop rbx\n"                                                    \
+                "add rsp, 8\n" /* Skip popping rsp, meaningless */             \
+                "pop rbp\n"                                                    \
+                "pop rsi\n"                                                    \
+                "pop rdi\n"                                                    \
+                "pop r8\n"                                                     \
+                "pop r9\n"                                                     \
+                "pop r10\n"                                                    \
+                "pop r11\n"                                                    \
+                "pop r12\n"                                                    \
+                "pop r13\n"                                                    \
+                "pop r14\n"                                                    \
+                "pop r15\n"                                                    \
+                "popf\n"                                                       \
+                                                                               \
+                "add rsp, 8\n" /* Padding before the SSE (stack aligned) */    \
+                "fxrstor [rsp]\n"                                              \
+                "add rsp, 512\n"                                               \
+                                                                               \
+            ::: "memory")
