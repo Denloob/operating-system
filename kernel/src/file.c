43b8e5de0003302d9d17ccb0d9043a46c79a9e77
Denloob <den@denloob.uk>
2024-12-17 13:19:55 +0200

    feat: support fread/fwrite to character devices

--- kernel/src/file.c
+++ kernel/src/file.c
@@ -1,9 +1,19 @@
 #include "file.h"
 #include "assert.h"
+#include "char_device.h"
 
 size_t fread(void *ptr, size_t size, size_t count, FILE *stream)
 {
-    size_t bytes_read = fat16_read(&stream->file, ptr, size * count, stream->offset);
+    size_t bytes_read = 0;
+    switch (fat16_get_mdscore_flags(&stream->file))
+    {
+        case fat16_MDSCoreFlags_FILE:
+            bytes_read = fat16_read(&stream->file, ptr, size * count, stream->offset);
+            break;
+        case fat16_MDSCoreFlags_DEVICE:
+            bytes_read = char_device_read(&stream->file, ptr, size * count, stream->offset);
+            break;
+    }
     stream->offset += bytes_read;
 
     return bytes_read / size;
@@ -11,7 +21,16 @@ size_t fread(void *ptr, size_t size, size_t count, FILE *stream)
 
 size_t fwrite(void *ptr, size_t size, size_t count, FILE *stream)
 {
-    size_t bytes_written = fat16_write(&stream->file, ptr , size * count, stream->offset);
+    size_t bytes_written = 0;
+    switch (fat16_get_mdscore_flags(&stream->file))
+    {
+        case fat16_MDSCoreFlags_FILE:
+            bytes_written = fat16_write(&stream->file, ptr , size * count, stream->offset);
+            break;
+        case fat16_MDSCoreFlags_DEVICE:
+            bytes_written = char_device_write(&stream->file, ptr, size * count, stream->offset);
+            break;
+    }
     stream->offset += bytes_written;
 
     return bytes_written / size;
@@ -19,6 +38,7 @@ size_t fwrite(void *ptr, size_t size, size_t count, FILE *stream)
 
 int fseek(FILE *stream, int64_t offset, file_Whence whence)
 {
+    // TODO: fseek for chr device
     uint64_t new_offset;
     switch (whence)
     {
