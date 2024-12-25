c5eb90736c469b63ed137619b29cad49d87f2739
maroms <maromshpak094@gmail.com>
2024-12-25 16:41:28 +0200

    feat: function to create a program from code

--- kernel/src/program.h
+++ kernel/src/program.h
@@ -2,4 +2,6 @@
 #include "elf.h"
 #include "res.h"
 
-res program_setup(uint64_t id,  PCB *parent, mmu_PageMapEntry *kernel_pml, fat16_Ref *fat16, const char *path_to_file);
+res program_setup_from_drive(uint64_t id,  PCB *parent, mmu_PageMapEntry *kernel_pml, fat16_Ref *fat16, const char *path_to_file);
+
+res program_setup_from_code(uint64_t id, PCB *parent, mmu_PageMapEntry *kernel_pml, void (*entry_point)(void));
