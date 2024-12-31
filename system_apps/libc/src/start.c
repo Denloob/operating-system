e9453add1c8dc82fedb4e95ce06ac33b1736883e
Denloob <den@denloob.uk>
2024-12-31 21:03:32 +0200

    feat: inititalize standard streams before processes start

--- system_apps/libc/src/start.c
+++ system_apps/libc/src/start.c
@@ -1,15 +1,29 @@
+#include <stdint.h>
+#include <stdio.h>
+
 extern int __attribute__((sysv_abi, used)) main(int argc, char **argv);
+
+static void __attribute__((used)) init_stdio_streams()
+{
+    stdin  = fopen("tty.dev");
+    stdout = fopen("tty.dev");
+    stderr = fopen("tty.dev");
+}
 
 void __attribute__((naked)) _start()
 {
     asm(
-        "pop rdi\n"       // `argc` is on the top of the stack
-        "mov rsi, rsp\n"  // `argv` comes right after it
+        "pop r14\n"       // `argc` is on the top of the stack
+        "mov r15, rsp\n"  // `argv` comes right after it
 
         // Align the stack to 16
         "sub rsp, 0x8\n"
         "and rsp, ~0x10\n"
 
+        "call init_stdio_streams\n"
+
+        "mov rdi, r14\n"
+        "mov rsi, r15\n"
         "call main\n" // call main(argc, argv)
 
         ".l: jmp .l\n" // TODO: when implemented, call exit syscall
