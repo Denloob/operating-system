49e3de02ebead5e3a6568534342f53a1ddaf8c4b
Denloob <den@denloob.uk>
2025-01-01 12:48:58 +0200

    refactor: remove unused program_setup_from_code

--- kernel/src/program.c
+++ kernel/src/program.c
@@ -127,32 +127,3 @@ res program_setup_from_drive(uint64_t id,  PCB *parent, mmu_PageMapEntry *kernel
 
     return res_OK;
 }
-
-
-res program_setup_from_code(uint64_t id, PCB *parent, mmu_PageMapEntry *kernel_pml, void (*entry_point)(void)) 
-{
-    //PCB handle
-    PCB *program_pcb = PCB_init(id, parent, 0, kernel_pml);
-
-    //switch PML
-    mmu_load_virt_pml4(program_pcb->paging);
-
-    //set rip
-    program_pcb->rip = (uint64_t)entry_point;
-
-    // Virtual memory consts for program stack
-    const size_t STACK_SIZE = PAGE_SIZE * 5;
-    const uint64_t STACK_VIRTUAL_BASE = 0x7FFFFFFFF000;
-
-    //Program stack handling
-    program_pcb->regs.rsp = STACK_VIRTUAL_BASE;
-    res rs = mmap((void *)(program_pcb->regs.rsp - STACK_SIZE), STACK_SIZE, MMAP_PROT_READ | MMAP_PROT_WRITE | MMAP_PROT_RING_3);
-    assert(IS_OK(rs));
-
-    //Set initial flags
-    program_pcb->regs.rflags = 0x202;
-
-    scheduler_process_enqueue(program_pcb);
-
-    return res_OK;
-}
--- kernel/src/program.h
+++ kernel/src/program.h
@@ -6,5 +6,3 @@
  * @param argv - The argv to copy into the process. Assume that `argv`'s content won't change (aka const). We can't specify so in C because of C limitations.
  */
 res program_setup_from_drive(uint64_t id,  PCB *parent, mmu_PageMapEntry *kernel_pml, fat16_Ref *fat16, const char *path_to_file, char **argv);
-
-res program_setup_from_code(uint64_t id, PCB *parent, mmu_PageMapEntry *kernel_pml, void (*entry_point)(void));
