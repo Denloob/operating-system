d3b3ad6cc426ece4b89bc4ca197808b26caec702
Denloob <den@denloob.uk>
2025-01-24 21:48:04 +0200

    feat: add mkdir command

---
+++ system_apps/mkdir/main.c
@@ -0,0 +1,21 @@
+#include <sys/stat.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{ 
+    if (argc < 2)
+    {
+        puts("mkdir: missing operand");
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        int ret = mkdir(argv[i]);
+        if (ret == -1)
+        {
+            printf("mkdir: %s: Couldn't create directory\n", argv[i]);
+        }
+    }
+
+    return 0;
+}
