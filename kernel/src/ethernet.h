8ebbfce07fd5ba6c42b2aeb13b9c6e86a3f8ab99
maroms <maromshpak094@gmail.com>
2025-02-07 12:48:11 +0200

    refactor: change structure names

--- kernel/src/ethernet.h
+++ kernel/src/ethernet.h
@@ -4,16 +4,18 @@
 
 #define ETHER_MAC_SIZE 6
 
-typedef struct __attribute__((packed)) {
-    uint8_t dest[ETHER_MAC_SIZE];
-    uint8_t source[ETHER_MAC_SIZE];
-    uint16_t type;
+typedef struct __attribute__((packed))
+{
+    uint8_t dest_mac[ETHER_MAC_SIZE];
+    uint8_t source_mac[ETHER_MAC_SIZE];
+    uint16_t ethertype;
     uint8_t data_and_fcs[]; // Data (46-1500 bytes) and the 4 byte fcs.
 } EthernetPacket;
 
 #define ETHER_PACKET_SIZE (sizeof(EthernetPacket) + 4) // +4 for fcs
 
-enum {
+enum 
+{
     ethernet_TYPE_IPv4 = 0x0800,
     ethernet_TYPE_ARP  = 0x0806,
 };
