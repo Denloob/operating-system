d9d1fe3d8d48a8f28e6abdf86137880574fc9fc9
Denloob <den@denloob.uk>
2025-02-07 22:35:20 +0200

    refactor: extract bit_test_set function into memory header

--- kernel/src/memory.h
+++ kernel/src/memory.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdint.h>
 #include "math.h"
 
 #define PAGE_SIZE 0x1000
@@ -12,3 +13,18 @@ int memcmp(const void *ptr1, const void *ptr2, size_t num);
 void *memset(void *dest, int val, int len);
 void *memchr(const void *s, int c, size_t n);
 void *memrchr(const void *s, int c, size_t n);
+
+__attribute__((always_inline))
+static inline uint8_t mem_bit_test_set(volatile uint32_t *val, int bit) // NOLINT: ignore false positive on `val` being a non-const pointer
+{
+    uint8_t ret;
+    asm volatile (
+        "lock bts %[val], %[bit]\n"
+        "setc %[ret]\n"
+        : [ret] "=r"(ret), [val] "+m"(*val)
+        : [bit] "r"(bit)
+    );
+
+    return ret;
+}
+
--- kernel/src/mutex.h
+++ kernel/src/mutex.h
@@ -2,29 +2,16 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include "memory.h"
 
 typedef volatile struct {
     uint32_t lock;
 } Mutex;
 
 __attribute__((always_inline))
-static inline uint8_t __mutex_bit_test_set(volatile uint32_t *val, int bit) // NOLINT: ignore false positive on `val` being a non-const pointer
-{
-    uint8_t ret;
-    asm volatile (
-        "lock bts %[val], %[bit]\n"
-        "setc %[ret]\n"
-        : [ret] "=r"(ret), [val] "+m"(*val)
-        : [bit] "r"(bit)
-    );
-
-    return ret;
-}
-
-__attribute__((always_inline))
 static inline bool mutex_trylock(Mutex *mutex)
 {
-    return __mutex_bit_test_set(&mutex->lock, 0) == 0;
+    return mem_bit_test_set(&mutex->lock, 0) == 0;
 }
 
 __attribute__((always_inline))
