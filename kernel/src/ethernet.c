198650f967f5d633ca364001006476caf9da3961
Denloob <den@denloob.uk>
2025-02-07 10:03:56 +0200

    fix: not converting packet type to host byte ordering

--- kernel/src/ethernet.c
+++ kernel/src/ethernet.c
@@ -1,10 +1,11 @@
 #include "ethernet.h"
+#include "net.h"
 #include "arp.h"
 #include "ipv4.h"
 
 void ethernet_handle_packet(EthernetPacket *packet, int data_length)
 {
-    switch (packet->type)
+    switch (ntohs(packet->type))
     {
         case ethernet_TYPE_ARP:
             arp_handle(packet, data_length);
