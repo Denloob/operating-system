43eac3f76d7c6abaa6005b2b64aa97ae50dcbaf7
Denloob <den@denloob.uk>
2025-03-21 19:44:47 +0200

    feat: add mouse button bitmask macros

--- system_apps/libgx/include/gx/mouse.h
+++ system_apps/libgx/include/gx/mouse.h
@@ -10,4 +10,8 @@ typedef struct __attribute__((packed)) {
 
 int gx_mouse_init();
 
+#define gx_mouse_BUTTON_LEFT   (1 << 0)
+#define gx_mouse_BUTTON_RIGHT  (1 << 1)
+#define gx_mouse_BUTTON_MIDDLE (1 << 2)
+
 gx_mouse_State gx_mouse_get_state() __attribute__((warn_unused_result));
