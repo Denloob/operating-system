c34ed10f88a59d413362da45e453e24991a5b5da
Denloob <den@denloob.uk>
2025-03-23 17:40:13 +0200

    feat: switch to vga text mode on assert fail

--- kernel/src/assert.c
+++ kernel/src/assert.c
@@ -1,1 +1,23 @@
-../../lib/assert.c
+#include "assert.h"
+#include "io.h"
+#include "vga.h"
+
+void assert_fail(const char *assertion, const char *file, const char *line,
+                 const char *function)
+{
+    if (vga_current_mode() != VGA_MODE_TYPE_TEXT)
+    {
+        vga_restore_default_color_palette();
+        vga_mode_text();
+        io_clear_vga();
+    }
+
+    put(file);
+    putc(':');
+    put(line);
+    put(": ");
+    put(function);
+    put(": Assertion failed: ");
+    puts(assertion);
+    cli_hlt();
+}
