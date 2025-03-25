499d9339e130ff25eedee11780643de8488f144e
Denloob <den@denloob.uk>
2025-03-25 10:05:48 +0200

    feat: allow executing shell commands via a shell argument

--- system_apps/sh/src/main.c
+++ system_apps/sh/src/main.c
@@ -12,19 +12,22 @@
 #include <dirent.h>
 #include <sys/syscall.h>
 
-void cd(ShellCommand *cmd)
+int cd(ShellCommand *cmd)
 {
     if (cmd->length != 2)
     {
         puts("cd: Invalid amount of args for cd command");
-        return;
+        return 1;
     }
 
     int ret = chdir(cmd->shell_command[1]);
     if (ret == -1)
     {
         printf("cd: %s: No such directory\n", cmd->shell_command[1]);
+        return 1;
     }
+
+    return 0;
 }
 
 #define MAX_JOB_COUNT 1024
@@ -78,18 +81,160 @@ bool jobs_insert(pid_t pid)
     return true;
 }
 
-int main()
-{
-    int8_t return_code = 0;
-
-    syscall(SYS_CreateWindow , 0);
 #define MAX_PATH_LENGTH 512
 
-    jobs_init();
+typedef struct {
+    int8_t return_code;
+    bool execution_successful;
+} ExecutionResult;
 
+ExecutionResult execute_expression(char *expr, size_t expr_length, char *path)
+{
+    ShellCommand *cmd = parser_parse_shell_expression(expr, expr_length);
+    if (cmd == NULL)
+    {
+        return (ExecutionResult){.execution_successful = false};
+    }
+    defer({ parser_release_parsed_expression(cmd); });
+
+    if (cmd->length == 0)
+    {
+        return (ExecutionResult){.execution_successful = false};
+    }
+
+    // Special case for built-in `cd` command
+    if (strcmp(cmd->shell_command[0], "cd") == 0)
+    {
+        int ret = cd(cmd);
+        return (ExecutionResult){.execution_successful = true, .return_code = ret};
+    }
+
+    pid_t pid = -1;
+    bool is_binary_path = strchr(cmd->shell_command[0], '/') != NULL;
+    if (is_binary_path)
+    {
+        pid = execve_new(cmd->shell_command[0], &cmd->shell_command[0]);
+    }
+    else
+    {
+        char *original_command = cmd->shell_command[0];
+        defer({ free(original_command); });
+        cmd->shell_command[0] = malloc(MAX_PATH_LENGTH);
+        if (cmd->shell_command[0] == NULL)
+        {
+            puts("Ran out of memory trying to run the command");
+
+            // defer will cleanup `cmd` and original_command. It's ok to free NULL.
+            return (ExecutionResult){.execution_successful = false};
+        }
+
+        size_t path_idx = 0;
+        do
+        {
+            if (path[path_idx] == '\0')
+            {
+                break;
+            }
+
+            char *path_end = strchrnul(&path[path_idx], ':');
+
+            size_t path_end_idx = path_end - path;
+            size_t path_len = path_end_idx - path_idx;
+            size_t path_begin = path_idx;
+
+            if (path[path_end_idx] == '\0')
+            {
+                path_idx = path_end_idx;
+            }
+            else
+            {
+                path_idx = path_end_idx + 1;
+            }
+
+            if (path_len == 0)
+            {
+                continue; // Skip
+            }
+
+            if (path_len + 1 >= MAX_PATH_LENGTH)
+            {
+                puts("Path too long, aborting");
+                break;
+            }
+
+            memmove(cmd->shell_command[0], &path[path_begin], path_len);
+            cmd->shell_command[0][path_len] = '/';
+            cmd->shell_command[0][path_len + 1] = '\0';
+            strncat(cmd->shell_command[0], original_command, MAX_PATH_LENGTH);
+            pid = execve_new(cmd->shell_command[0], &cmd->shell_command[0]);
+        }
+        while (pid == -1);
+    }
+
+    if (pid == -1)
+    {
+        printf("Unknown command: '%s'\n", cmd->shell_command[0]);
+        return (ExecutionResult){.execution_successful = false};
+    }
+
+    if (cmd->is_async)
+    {
+        jobs_refresh();
+        bool success = jobs_insert(pid);
+        if (!success)
+        {
+            puts("shcore: warning: jobs list is full, this process will be lost");
+        }
+        return (ExecutionResult){.execution_successful = true, .return_code = 0};
+    }
+
+    int wstatus;
+    pid_t child_pid = waitpid(pid, &wstatus, 0);
+    if (child_pid == -1)
+    {
+        printf("waitpid: something went wrong\n");
+        return (ExecutionResult){.execution_successful = true, .return_code = -1};
+    }
+
+    assert(WIFEXITED(wstatus));
+    int return_code = WEXITSTATUS(wstatus);
+    return (ExecutionResult){.execution_successful = true, .return_code = return_code};
+}
+
+int main(int argc, char **argv)
+{
 #define INITIAL_PATH_SIZE MAX_PATH_LENGTH
     char *path = calloc(1, INITIAL_PATH_SIZE);
     strcpy(path, "/bin");
+
+    if (argc > 1)
+    {
+        if (strcmp(argv[1], "-c"))
+        {
+            printf("shcore: %s: invalid options\n", argv[1]);
+            return 1;
+        }
+
+        if (argc < 2)
+        {
+            printf("shcore: %s: option requires an argument\n", argv[1]);
+            return 1;
+        }
+
+        ExecutionResult res = execute_expression(argv[2], strlen(argv[2]), path);
+        if (res.execution_successful)
+        {
+            return res.return_code;
+        }
+
+        return INT8_MAX;
+    }
+
+    int8_t return_code = 0;
+
+    jobs_init();
+
+    syscall(SYS_CreateWindow , 0);
 
 #define CWD_MAX_LEN MAX_PATH_LENGTH
     char cwd[CWD_MAX_LEN] = {0};
@@ -119,111 +264,10 @@ int main()
         buf[input_len - 1] = '\0';
         input_len--;
 
-        ShellCommand *cmd = parser_parse_shell_expression(buf, input_len);
-        if (cmd == NULL)
+        ExecutionResult res = execute_expression(buf, input_len, path);
+        if (res.execution_successful)
         {
-            continue;
-        }
-        defer({ parser_release_parsed_expression(cmd); });
-
-        if (cmd->length == 0)
-        {
-            continue;
-        }
-
-        // Special case for built-in `cd` command
-        if (strcmp(cmd->shell_command[0], "cd") == 0)
-        {
-            cd(cmd);
-            continue;
-        }
-
-        pid_t pid = -1;
-        bool is_binary_path = strchr(cmd->shell_command[0], '/') != NULL;
-        if (is_binary_path)
-        {
-            pid = execve_new(cmd->shell_command[0], &cmd->shell_command[0]);
-        }
-        else
-        {
-            char *original_command = cmd->shell_command[0];
-            defer({ free(original_command); });
-            cmd->shell_command[0] = malloc(MAX_PATH_LENGTH);
-            if (cmd->shell_command[0] == NULL)
-            {
-                puts("Ran out of memory trying to run the command");
-                continue; // defer will cleanup `cmd` and original_command. It's ok to free NULL.
-            }
-
-            size_t path_idx = 0;
-            do
-            {
-                if (path[path_idx] == '\0')
-                {
-                    break;
-                }
-
-                char *path_end = strchrnul(&path[path_idx], ':');
-
-                size_t path_end_idx = path_end - path;
-                size_t path_len = path_end_idx - path_idx;
-                size_t path_begin = path_idx;
-
-                if (path[path_end_idx] == '\0')
-                {
-                    path_idx = path_end_idx;
-                }
-                else
-                {
-                    path_idx = path_end_idx + 1;
-                }
-
-                if (path_len == 0)
-                {
-                    continue; // Skip
-                }
-
-                if (path_len + 1 >= MAX_PATH_LENGTH)
-                {
-                    puts("Path too long, aborting");
-                    break;
-                }
-
-                memmove(cmd->shell_command[0], &path[path_begin], path_len);
-                cmd->shell_command[0][path_len] = '/';
-                cmd->shell_command[0][path_len + 1] = '\0';
-                strncat(cmd->shell_command[0], original_command, MAX_PATH_LENGTH);
-                pid = execve_new(cmd->shell_command[0], &cmd->shell_command[0]);
-            }
-            while (pid == -1);
-        }
-
-        if (pid == -1)
-        {
-            printf("Unknown command: '%s'\n", cmd->shell_command[0]);
-        }
-        else if (!cmd->is_async)
-        {
-            int wstatus;
-            pid_t child_pid = waitpid(pid, &wstatus, 0);
-            if (child_pid == -1)
-            {
-                printf("waitpid: something went wrong\n");
-            }
-            else
-            {
-                assert(WIFEXITED(wstatus));
-                return_code = WEXITSTATUS(wstatus);
-            }
-        }
-        else
-        {
-            jobs_refresh();
-            bool success = jobs_insert(pid);
-            if (!success)
-            {
-                puts("shcore: warning: jobs list is full, this process will be lost");
-            }
+            return_code = res.return_code;
         }
     }
 }
