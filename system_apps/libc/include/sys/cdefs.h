fab5789286d6d9bbe2377ee34abc54aa7f6619cf
Denloob <den@denloob.uk>
2025-03-26 17:17:31 +0200

    feat: add needed sys cdefs defines

--- system_apps/libc/include/sys/cdefs.h
+++ system_apps/libc/include/sys/cdefs.h
@@ -1,5 +1,5 @@
-#pragma once
-
+#ifndef	_SYS_CDEFS_H
+#define	_SYS_CDEFS_H	1
 
 #define _WUR __attribute__((warn_unused_result))
 
@@ -7,4 +7,20 @@
     #define __attribute_malloc(deallocator) __attribute__((malloc))
 #else
     #define __attribute_malloc(deallocator) __attribute__((malloc(deallocator)))
+#endif
+
+#define __LEAF , __leaf__
+#define __LEAF_ATTR __attribute__ ((__leaf__))
+
+#define __THROW	__attribute__ ((__nothrow__ __LEAF))
+#define __THROWNL	__attribute__ ((__nothrow__))
+#define __NTH(fct)	__attribute__ ((__nothrow__ __LEAF)) fct
+#define __NTHNL(fct)  __attribute__ ((__nothrow__)) fct
+
+#define __BEGIN_DECLS
+#define __END_DECLS
+
+#define __attribute_nonnull__(params) __attribute__ ((__nonnull__ params))
+#define __nonnull(params) __attribute_nonnull__ (params)
+
 #endif
